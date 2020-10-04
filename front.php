d4300bb3520d8e5f3496e8083d2fbecba32b6eb5
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2020-10-04 08:18:48 -0700

    daily commit

--- front.php
+++ front.php
@@ -1,11 +1,11 @@
 <?php
-	// Template Name: Home
+    // Template Name: Home
 ?>
 
 <?php get_header();?>
 
 
-<?php # Welcome Scheme ?>
+<?php # Welcome Schene?>
 <div class="welcome">
     <div class="overlay"></div>
     <div class="home-video"></div>
@@ -21,7 +21,7 @@
 </div>
 
 
-<?php # Homepage  Music, Events ?>
+<?php # Homepage  Music, Events?>
 <main class="interior" id="#skiptomaincontent">
     <div class="content">
        
@@ -32,27 +32,27 @@
 
         <h2 class="color-2" id="listen"><i class="fas fa-compact-disc"></i> Listen to House Music</h2>
         <article class="set-of-posts">
-            <?php 
-			$args = array(
-				'posts_per_page' => 12,
-				'cat' => 22,
-				'post_status'=>"publish",
-				'post_type'=>"post",
-				// 'orderby'=>"rand", 
-				// 'year' => "2020, 2019"
-			);
-			$query1 = new WP_Query( $args );
-			?>
-            <?php if ( $query1 -> have_posts() ) : while ( $query1 -> have_posts() ) : $query1 -> the_post();?>
-            <div class="excerpt bg-color-<?php echo rand(1,6) ?>">
+            <?php
+            $args = array(
+                'posts_per_page' => 12,
+                'cat' => 22,
+                'post_status'=>"publish",
+                'post_type'=>"post",
+                // 'orderby'=>"rand",
+                // 'year' => "2020, 2019"
+            );
+            $query1 = new WP_Query($args);
+            ?>
+            <?php if ($query1 -> have_posts()) : while ($query1 -> have_posts()) : $query1 -> the_post();?>
+            <div class="excerpt bg-color-<?php echo rand(1, 6) ?>">
                 <div class="thumb">
                     <a href="<?php the_permalink(); ?>" title="<?php the_title();?>">
                         <?php the_post_thumbnail('', array('class' => 'b-lazy')); ?>
                     </a>
                 </div>
-                <h3 class="blog-title color-<?php echo rand(1,6) ?>"><a href="<?php the_permalink();?>"
+                <h3 class="blog-title color-<?php echo rand(1, 6) ?>"><a href="<?php the_permalink();?>"
                         title="<?php the_title();?>"><?php the_title();?></a></h3>
-                <?php if(!empty(get_the_post_thumbnail())): ?>
+                <?php if (!empty(get_the_post_thumbnail())): ?>
                 <div class="meta-data">
                     <i class="fas fa-clock"></i> <?php the_time('M');?> <?php the_time('j');?>, <?php the_time('Y'); ?>
                     <i class="fas fa-headphones"></i> <?php the_category(', '); ?>
@@ -114,7 +114,6 @@
         <br clear="all">
 
 
-    </div>
 
 
 
@@ -124,30 +123,30 @@
         <h2 class="color-2"><i class="fas fa-compact-disc"></i> DJ Mixes from the Archives</h2>
 
         <article class="set-of-posts owl-rotator owl-carousel">
-            <?php 
-			
-			$args = array(
-				'posts_per_page' => 10,
-				'cat' => 22,
-				'post_status'=>"publish",
-				'post_type'=>"post",
-				'orderby'=>"rand", 
-				'year' => "2019"
-			);
+            <?php
+            
+            $args = array(
+                'posts_per_page' => 10,
+                'cat' => 22,
+                'post_status'=>"publish",
+                'post_type'=>"post",
+                'orderby'=>"rand",
+                'year' => "2019"
+            );
 
-			$query1 = new WP_Query( $args ); ?>
+            $query1 = new WP_Query($args); ?>
 
-            <?php if ( $query1 -> have_posts() ) : while ( $query1 -> have_posts() ) : $query1 -> the_post();?>
-            <div class="excerpt-rotator bg-color-<?php echo rand(1,6) ?>">
+            <?php if ($query1 -> have_posts()) : while ($query1 -> have_posts()) : $query1 -> the_post();?>
+            <div class="excerpt-rotator bg-color-<?php echo rand(1, 6) ?>">
 
-                <?php if(!empty(get_the_post_thumbnail())): ?>
+                <?php if (!empty(get_the_post_thumbnail())): ?>
                 <div class="thumb">
                     <a href="<?php the_permalink(); ?>" title="<?php the_title();?>">
                         <?php the_post_thumbnail('', array('class' => 'b-lazy')); ?>
                     </a>
                 </div>
                 <?php endif; ?>
-                <h3 class="blog-title color-<?php echo rand(1,6) ?>"><a href="<?php the_permalink();?>"
+                <h3 class="blog-title color-<?php echo rand(1, 6) ?>"><a href="<?php the_permalink();?>"
                         title="<?php the_title();?>"><?php the_title();?></a></h3>
                 <div class="meta-data">
                     <i class="fas fa-clock"></i> <?php the_time('M');?> <?php the_time('j');?>, <?php the_time('Y'); ?>
@@ -177,6 +176,9 @@
         <a href="<?php bloginfo('url'); ?>/category/music/" rel="nofollow" class="button">More Music</a>
         <a href="<?php bloginfo('url'); ?>/submit-your-dj-mix/" rel="nofollow" class="button">Submit DJ Mix</a>
         </div>
+
+    </div>
+
 </main>
 
 <?php get_footer();?>
