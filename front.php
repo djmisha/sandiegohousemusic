37b72f407dbf27adbed2c3b76d202579370d1f31
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2021-09-29 17:51:15 -0700

    spelling

--- front.php
+++ front.php
@@ -101,7 +101,7 @@
       <p>Find dance music, edm shows and house music events in a city near you. </p>
   
     <div class="front-page-events">
-        <iframe class="b-lazy" data-src="https://sandiegohousemusic.com/events/shdm-homepage.html" frameborder="0" width="100%"
+        <iframe class="b-lazy" data-src="https://sandiegohousemusic.com/events/sdhm-homepage.html" frameborder="0" width="100%"
             height="1320" style="overflow: hidden;" scrolling="no"></iframe>
     </div>
 
