1647c70fc399bf79e16a82470b978ba9c94df44a
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2023-04-27 10:59:45 +0000

    remove google tag

--- footer.php
+++ footer.php
@@ -155,19 +155,5 @@
 
     <?php endif; ?>
 
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115514301-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'UA-115514301-1');
-    </script>
-
 </body>
 </html>
